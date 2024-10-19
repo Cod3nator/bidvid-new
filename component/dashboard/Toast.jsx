80f8cff6440e9a6d326ffd12bad236b50e45a9af
cod3nator <asishbosek@gmail.com>
2024-10-19 16:48:38 +0530

    Changes on toast component for error

--- component/dashboard/Toast.jsx
+++ component/dashboard/Toast.jsx
@@ -16,25 +16,34 @@ const Toast = ({ success, message }) => {
   // }, []);
 
   if (!visible) return null; 
-
+  const toastColor={
+    success:{
+      bg:"#4BB543",
+    },
+    failure:{
+      bg:"#ff5d33",
+    }
+  }
   return (
-    <div className={`absolute transition-transform duration-500 ${visible ? 'translate-x-1/2' : 'translate-x-full'}`} style={{right:"10px",bottom:"20px"}}>
+    <div className={`absolute transition-transform duration-500  rounded-lg ${visible ? 'translate-x-1/2' : 'translate-x-full'}`} style={{right:"10px",bottom:"20px"}}>
+     
       <div
-        className={`max-w-xs ${success ? 'bg-teal-100 border-teal-200  text-teal-800' : 'bg-red-100 border-red-200 text-red-800'} border text-sm rounded-lg dark:bg-red-800/10 dark:border-red-900 dark:text-red-500`}
+        className={`max-w-xs flex  border text-sm rounded-lg`} style={{backgroundColor: success ? toastColor.success.bg : toastColor.failure.bg, border:"none",color:'#fff'
+        }}
         role="alert"
         tabIndex="-1"
         aria-labelledby="hs-toast-soft-color-label"
       >
-        <div id="hs-toast-soft-color-label" className="flex p-4">
+        <div id="hs-toast-soft-color-label" className="flex p-4  flex justify-center items-center">
           {message}
-          <div className="ms-auto">
+          <div className="flex justify-center items-center" style={{marginLeft: '5px'}}>
             <button
               type="button"
               className="inline-flex shrink-0 justify-center items-center size-5 rounded-lg text-red-800 opacity-50 hover:opacity-100 focus:outline-none focus:opacity-100 dark:text-red-200"
               aria-label="Close"
               onClick={handleClose}
             >
-              <span className="sr-only">Close</span>
+           
               <svg
                 className="shrink-0 size-4"
                 xmlns="http://www.w3.org/2000/svg"
