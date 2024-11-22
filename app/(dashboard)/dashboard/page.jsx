99f22c073f2ad9438260d4b4aace5f5b5506f141
cod3nator <asishbosek@gmail.com>
2024-11-22 16:44:48 +0530

    Changes on dashboard from funtion to page

--- app/(dashboard)/dashboard/page.jsx
+++ app/(dashboard)/dashboard/page.jsx
@@ -1,24 +1,37 @@
 "use client"
 import React, { useEffect, useState } from "react";
-
 import ClientDashboard from "../components/ClientDashboard";
 
 export const dynamic = 'force-dynamic';
 
+const DashboardPage = () => {
+  const [campaignData, setCampaignData] = useState([]);
+  const [headers, setHeaders] = useState([]);
 
-const DashboardPage = async() => {
-  const campaignData = await fetch('https://devapi.bidvid.in/campaign_reports', {
-    cache: 'no-store', 
-  }).then((res) => res.json());
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://devapi.bidvid.in/campaign_reports', {
+          cache: 'no-store',
+        });
+        const data = await response.json();
+        setCampaignData(data);
+        
+        const headers = Object.keys(data[0]).map((key) =>
+          key.replace(/_/g, ' ').replace(/\b\w/g, (char) => char.toUpperCase())
+        );
+        setHeaders(headers);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    
+    fetchData();
+  }, []);
 
-  const headers = Object.keys(campaignData[0]).map((key) =>
-    key.replace(/_/g, ' ').replace(/\b\w/g, (char) => char.toUpperCase())
-  );
- 
   return (
-  <ClientDashboard campaignData={campaignData} headers={headers} />
+    <ClientDashboard campaignData={campaignData} headers={headers} />
   );
 };
-
 
 export default DashboardPage;
